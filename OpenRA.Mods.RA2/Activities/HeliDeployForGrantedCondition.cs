2f2e076503472bfb2e6319ba5103bcce7758d0f3
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2019-02-19 19:11:12 +0300

    Add HeliGrantConditionOnDeploy and use it for SiegeChopper
    Deploy anim don't work properly will need help.

---
+++ OpenRA.Mods.RA2/Activities/HeliDeployForGrantedCondition.cs
@@ -0,0 +1,84 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2019 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System.Linq;
+using OpenRA.Activities;
+using OpenRA.Mods.Common.Activities;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Mods.RA2.Traits;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.RA2.Activities
+{
+	public class HeliDeployForGrantedCondition : Activity
+	{
+        readonly World w;
+        readonly Aircraft aircraft;
+		readonly HeliGrantConditionOnDeploy deploy;
+		readonly bool canTurn;
+
+		public HeliDeployForGrantedCondition(Actor self, HeliGrantConditionOnDeploy deploy)
+		{
+            w = self.World;
+            this.deploy = deploy;
+            aircraft = self.Trait<Aircraft>();
+			canTurn = self.Info.HasTraitInfo<IFacingInfo>();
+		}
+
+		protected override void OnFirstRun(Actor self)
+		{
+            if (!aircraft.CanLand(w.Map.CellContaining(self.CenterPosition)))
+            {
+                var cells = w.Map.AllCells.Where(c => aircraft.CanLand(c)).Select(c => w.Map.CenterOfCell(c));
+                var cell = w.Map.CellContaining(WorldUtils.PositionClosestTo(cells, self.CenterPosition));
+
+                QueueChild(ActivityUtils.SequenceActivities(new HeliFly(self, Target.FromCell(w, cell))));
+            }
+
+            // Turn to the required facing.
+            if (deploy.Info.Facing != -1 && canTurn)
+				QueueChild(new Turn(self, deploy.Info.Facing));
+
+            QueueChild(new HeliLand(self, true));
+        }
+
+		public override Activity Tick(Actor self)
+		{
+			// Do turn first, if needed.
+			if (ChildActivity != null)
+			{
+				ChildActivity = ActivityUtils.RunActivity(self, ChildActivity);
+				return this;
+			}
+
+			// Without this, turn for facing deploy angle will be canceled and immediately deploy!
+			if (IsCanceled)
+				return NextActivity;
+
+			if (IsInterruptible)
+			{
+				IsInterruptible = false; // must DEPLOY from now.
+				deploy.Deploy();
+				return this;
+			}
+
+			// Wait for deployment
+			if (deploy.DeployState == DeployState.Deploying)
+				return this;
+
+			// Failed or success, we are going to NextActivity.
+			// Deploy() at the first run would have put DeployState == Deploying so
+			// if we are back to DeployState.Undeployed, it means deploy failure.
+			// Parent activity will see the status and will take appropriate action.
+			return NextActivity;
+		}
+	}
+}
---
+++ OpenRA.Mods.RA2/Traits/Conditions/HeliGrantConditionOnDeploy.cs
@@ -0,0 +1,294 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2019 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System.Collections.Generic;
+using System.Linq;
+using OpenRA.Mods.RA2.Activities;
+using OpenRA.Mods.Common.Orders;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.RA2.Traits
+{
+	[Desc("Grants a condition when a deploy order is issued." +
+		"Can be paused with the granted condition to disable undeploying.")]
+	public class HeliGrantConditionOnDeployInfo : PausableConditionalTraitInfo, Requires<AircraftInfo>
+	{
+		[GrantedConditionReference]
+		[Desc("The condition to grant while the actor is undeployed.")]
+		public readonly string UndeployedCondition = null;
+
+		[FieldLoader.Require]
+		[GrantedConditionReference]
+		[Desc("The condition to grant after deploying and revoke before undeploying.")]
+		public readonly string DeployedCondition = null;
+
+		[Desc("The terrain types that this actor can deploy on. Leave empty to allow any.")]
+		public readonly HashSet<string> AllowedTerrainTypes = new HashSet<string>();
+
+		[Desc("Can this actor deploy on slopes?")]
+		public readonly bool CanDeployOnRamps = false;
+
+		[Desc("Cursor to display when able to (un)deploy the actor.")]
+		public readonly string DeployCursor = "deploy";
+
+		[Desc("Cursor to display when unable to (un)deploy the actor.")]
+		public readonly string DeployBlockedCursor = "deploy-blocked";
+
+		[Desc("Facing that the actor must face before deploying. Set to -1 to deploy regardless of facing.")]
+		public readonly int Facing = -1;
+
+		[Desc("Sound to play when deploying.")]
+		public readonly string DeploySound = null;
+
+		[Desc("Sound to play when undeploying.")]
+		public readonly string UndeploySound = null;
+
+		[Desc("Skip make/deploy animation?")]
+		public readonly bool SkipMakeAnimation = false;
+
+		public override object Create(ActorInitializer init) { return new HeliGrantConditionOnDeploy(init, this); }
+	}
+
+	public class HeliGrantConditionOnDeploy : PausableConditionalTrait<HeliGrantConditionOnDeployInfo>, IResolveOrder, IIssueOrder, INotifyCreated,
+		INotifyDeployComplete, IIssueDeployOrder
+	{
+		readonly Actor self;
+		readonly bool checkTerrainType;
+		readonly bool canTurn;
+
+		DeployState deployState;
+		ConditionManager conditionManager;
+		INotifyDeployTriggered[] notify;
+		int deployedToken = ConditionManager.InvalidConditionToken;
+		int undeployedToken = ConditionManager.InvalidConditionToken;
+
+		public DeployState DeployState { get { return deployState; } }
+
+		public HeliGrantConditionOnDeploy(ActorInitializer init, HeliGrantConditionOnDeployInfo info)
+			: base(info)
+		{
+			self = init.Self;
+			checkTerrainType = info.AllowedTerrainTypes.Count > 0;
+			canTurn = self.Info.HasTraitInfo<IFacingInfo>();
+			if (init.Contains<DeployStateInit>())
+				deployState = init.Get<DeployStateInit, DeployState>();
+		}
+
+		void INotifyCreated.Created(Actor self)
+		{
+			conditionManager = self.TraitOrDefault<ConditionManager>();
+			notify = self.TraitsImplementing<INotifyDeployTriggered>().ToArray();
+
+			switch (deployState)
+			{
+				case DeployState.Undeployed:
+					OnUndeployCompleted();
+					break;
+				case DeployState.Deploying:
+					if (canTurn)
+						self.Trait<IFacing>().Facing = Info.Facing;
+
+					Deploy(true);
+					break;
+				case DeployState.Deployed:
+					if (canTurn)
+						self.Trait<IFacing>().Facing = Info.Facing;
+
+					OnDeployCompleted();
+					break;
+				case DeployState.Undeploying:
+					if (canTurn)
+						self.Trait<IFacing>().Facing = Info.Facing;
+
+					Undeploy(true);
+					break;
+			}
+		}
+
+		public IEnumerable<IOrderTargeter> Orders
+		{
+			get
+			{
+				if (!IsTraitDisabled)
+					yield return new DeployOrderTargeter("HeliGrantConditionOnDeploy", 5,
+						() => IsCursorBlocked() ? Info.DeployBlockedCursor : Info.DeployCursor);
+			}
+		}
+
+		public Order IssueOrder(Actor self, IOrderTargeter order, Target target, bool queued)
+		{
+			if (order.OrderID == "HeliGrantConditionOnDeploy")
+				return new Order(order.OrderID, self, queued);
+
+			return null;
+		}
+
+		Order IIssueDeployOrder.IssueDeployOrder(Actor self, bool queued)
+		{
+			return new Order("HeliGrantConditionOnDeploy", self, queued);
+		}
+
+		bool IIssueDeployOrder.CanIssueDeployOrder(Actor self) { return !IsTraitPaused && !IsTraitDisabled; }
+
+		public void ResolveOrder(Actor self, Order order)
+		{
+			if (IsTraitDisabled || IsTraitPaused)
+				return;
+
+			if (order.OrderString != "HeliGrantConditionOnDeploy" || deployState == DeployState.Deploying || deployState == DeployState.Undeploying)
+				return;
+
+			if (!order.Queued)
+				self.CancelActivity();
+
+			if (deployState == DeployState.Deployed)
+				self.QueueActivity(new HeliUndeployForGrantedCondition(self, this));
+			else if (deployState == DeployState.Undeployed)
+				self.QueueActivity(new HeliDeployForGrantedCondition(self, this));
+		}
+
+		bool IsCursorBlocked()
+		{
+			if (IsTraitPaused)
+				return true;
+
+			return !IsValidTerrain(self.Location) && (deployState != DeployState.Deployed);
+		}
+
+		public bool IsValidTerrain(CPos location)
+		{
+			return IsValidTerrainType(location) && IsValidRampType(location);
+		}
+
+		bool IsValidTerrainType(CPos location)
+		{
+			if (!self.World.Map.Contains(location))
+				return false;
+
+			if (!checkTerrainType)
+				return true;
+
+			var terrainType = self.World.Map.GetTerrainInfo(location).Type;
+
+			return Info.AllowedTerrainTypes.Contains(terrainType);
+		}
+
+		bool IsValidRampType(CPos location)
+		{
+			if (Info.CanDeployOnRamps)
+				return true;
+
+			var ramp = 0;
+			if (self.World.Map.Contains(location))
+			{
+				var tile = self.World.Map.Tiles[location];
+				var ti = self.World.Map.Rules.TileSet.GetTileInfo(tile);
+				if (ti != null)
+					ramp = ti.RampType;
+			}
+
+			return ramp == 0;
+		}
+
+		void INotifyDeployComplete.FinishedDeploy(Actor self)
+		{
+			OnDeployCompleted();
+		}
+
+		void INotifyDeployComplete.FinishedUndeploy(Actor self)
+		{
+			OnUndeployCompleted();
+		}
+
+		/// <summary>Play deploy sound and animation.</summary>
+		public void Deploy() { Deploy(false); }
+		void Deploy(bool init)
+		{
+			// Something went wrong, most likely due to deploy order spam and the fact that this is a delayed action.
+			if (!init && deployState != DeployState.Undeployed)
+				return;
+
+			if (!IsValidTerrain(self.Location))
+				return;
+
+			if (!string.IsNullOrEmpty(Info.DeploySound))
+				Game.Sound.Play(SoundType.World, Info.DeploySound, self.CenterPosition);
+
+			// Revoke condition that is applied while undeployed.
+			if (!init)
+				OnDeployStarted();
+
+			// If there is no animation to play just grant the condition that is used while deployed.
+			// Alternatively, play the deploy animation and then grant the condition.
+			if (!notify.Any())
+				OnDeployCompleted();
+			else
+				foreach (var n in notify)
+					n.Deploy(self, Info.SkipMakeAnimation);
+		}
+
+		/// <summary>Play undeploy sound and animation and after that revoke the condition.</summary>
+		public void Undeploy() { Undeploy(false); }
+		void Undeploy(bool init)
+		{
+			// Something went wrong, most likely due to deploy order spam and the fact that this is a delayed action.
+			if (!init && deployState != DeployState.Deployed)
+				return;
+
+			if (!string.IsNullOrEmpty(Info.UndeploySound))
+				Game.Sound.Play(SoundType.World, Info.UndeploySound, self.CenterPosition);
+
+			if (!init)
+				OnUndeployStarted();
+
+			// If there is no animation to play just grant the condition that is used while undeployed.
+			// Alternatively, play the undeploy animation and then grant the condition.
+			if (!notify.Any())
+				OnUndeployCompleted();
+			else
+				foreach (var n in notify)
+					n.Undeploy(self, Info.SkipMakeAnimation);
+		}
+
+		void OnDeployStarted()
+		{
+			if (undeployedToken != ConditionManager.InvalidConditionToken)
+				undeployedToken = conditionManager.RevokeCondition(self, undeployedToken);
+
+			deployState = DeployState.Deploying;
+		}
+
+		void OnDeployCompleted()
+		{
+			if (conditionManager != null && !string.IsNullOrEmpty(Info.DeployedCondition) && deployedToken == ConditionManager.InvalidConditionToken)
+				deployedToken = conditionManager.GrantCondition(self, Info.DeployedCondition);
+
+			deployState = DeployState.Deployed;
+		}
+
+		void OnUndeployStarted()
+		{
+			if (deployedToken != ConditionManager.InvalidConditionToken)
+				deployedToken = conditionManager.RevokeCondition(self, deployedToken);
+
+			deployState = DeployState.Deploying;
+		}
+
+		void OnUndeployCompleted()
+		{
+			if (conditionManager != null && !string.IsNullOrEmpty(Info.UndeployedCondition) && undeployedToken == ConditionManager.InvalidConditionToken)
+				undeployedToken = conditionManager.GrantCondition(self, Info.UndeployedCondition);
+
+			deployState = DeployState.Undeployed;
+		}
+	}
+}
