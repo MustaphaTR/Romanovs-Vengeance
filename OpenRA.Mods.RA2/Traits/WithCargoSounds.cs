cc4cf45178f40a4c27a813aa3d9aac5ff229fb52
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2020-01-21 13:57:34 +0300

    Implement WithCargoSounds.

---
+++ OpenRA.Mods.RA2/Traits/WithCargoSounds.cs
@@ -0,0 +1,56 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2007-2019 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.RA2.Traits
+{
+	public class WithCargoSoundsInfo : ConditionalTraitInfo, Requires<CargoInfo>
+	{
+		[Desc("Speech notification played when the first actor enters this garrison.")]
+		public readonly string EnterNotification = null;
+
+		[Desc("Speech notification played when the last actor leaves this garrison.")]
+		public readonly string ExitNotification = null;
+
+		[Desc("Sound played when the first actor enters this garrison.")]
+		public readonly string EnterSound = null;
+
+		[Desc("Sound played when the last actor exits this garrison.")]
+		public readonly string ExitSound = null;
+
+		public override object Create(ActorInitializer init) { return new WithCargoSounds(init.Self, this); }
+	}
+
+	public class WithCargoSounds : ConditionalTrait<WithCargoSoundsInfo>, INotifyPassengerEntered, INotifyPassengerExited
+	{
+		readonly Cargo cargo;
+
+		public WithCargoSounds(Actor self, WithCargoSoundsInfo info)
+            : base(info)
+		{
+			cargo = self.Trait<Cargo>();
+		}
+
+		void INotifyPassengerEntered.OnPassengerEntered(Actor self, Actor passenger)
+		{
+			Game.Sound.Play(SoundType.World, Info.EnterSound, self.CenterPosition);
+			Game.Sound.PlayNotification(self.World.Map.Rules, passenger.Owner, "Speech", Info.EnterNotification, passenger.Owner.Faction.InternalName);
+		}
+
+		void INotifyPassengerExited.OnPassengerExited(Actor self, Actor passenger)
+		{
+			Game.Sound.Play(SoundType.World, Info.ExitSound, self.CenterPosition);
+			Game.Sound.PlayNotification(self.World.Map.Rules, passenger.Owner, "Speech", Info.ExitNotification, passenger.Owner.Faction.InternalName);
+		}
+	}
+}
