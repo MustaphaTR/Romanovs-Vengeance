6f93a1786ae33a8388629119be7b6db723fb38cf
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2022-06-07 10:06:43 +0300

    Remove cathosp.
    I haven't checked the other maps yet. Will update later.

--- OpenRA.Mods.RA2/UtilityCommands/ImportRA2MapCommand.cs
+++ OpenRA.Mods.RA2/UtilityCommands/ImportRA2MapCommand.cs
@@ -1,6 +1,6 @@
 #region Copyright & License Information
 /*
- * Copyright 2007-2018 The OpenRA Developers (see AUTHORS)
+ * Copyright 2007-2022 The OpenRA Developers (see AUTHORS)
  * This file is part of OpenRA, which is free software. It is made
  * available to you under the terms of the GNU General Public License
  * as published by the Free Software Foundation, either version 3 of
@@ -362,9 +362,12 @@ namespace OpenRA.Mods.Cnc.UtilityCommands
 		static readonly Dictionary<string, string> ReplaceActors = new Dictionary<string, string>()
 		{
 			{ "amradr", "gaairc" },
+			{ "cathosp", "cahosp" },
 			{ "sengineer", "engineer" },
 			{ "yengineer", "engineer" },
 			{ "adog", "dog" },
+			{ "ydog", "dog" },
+			{ "yadog", "dog" },
 			{ "ingalite", "galite.white" },
 			{ "neglamp", "galite.black" },
 			{ "inredlmp", "galite.red" },
